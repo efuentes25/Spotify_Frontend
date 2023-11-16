5b5d3e323836725bc87e64613bff7bb6ad47b7e7
Edgar <edgarfuentes704@gmail.com>
2023-11-15 20:48:07 -0800

    implemented simple search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-// App.js - Main file where you use MusicApp component
-import React from 'react';
+import './App.css';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import PlaylistDisplay from './components/PlaylistDisplay';
+import SearchBar from './components/SearchBar';
 import MusicApp from './MusicApp';
 
-const App = () => {
+function App() {
   return (
     <div>
       <MusicApp />
     </div>
   );
-};
+}
+
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const SERVER_URL = 'http://localhost:8080';
+
+export const SEMESTERS = [
+  { year:2020, semester:'Fall'},
+  { year:2021, semester:'Spring'},
+  { year:2021, semester:'Fall'}
+] ;
