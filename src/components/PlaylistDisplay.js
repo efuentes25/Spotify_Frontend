b045b4fb9b0fb7a5edb8d6f7ff9eab7422257027
Josue-SR <santozjosue831@gmail.com>
2023-11-15 23:00:18 -0800

    Styled the search playlist

--- src/components/PlaylistDisplay.js
+++ src/components/PlaylistDisplay.js
@@ -1,20 +1,44 @@
 // PlaylistDisplay.js - This component will display the playlists based on the search
 import React from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 
-const PlaylistDisplay = ({ playlists }) => {
+const PlaylistDisplay = ({ playlists, isVisible }) => {
+  if (!isVisible) {
+    return null; // Return null to render nothing when not visible
+  }
   return (
-    <div>
-      <h2>Playlists:</h2>
-      <ul>
-        {playlists.map((playlist) => (
-          <li key={playlist.id}>
-            <button onClick={() => console.log(`Clicked playlist: ${playlist.name}`)}>
-              {playlist.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container className="mt-4">
+      <Row>
+        <Col>
+          <Card bg="dark" text="white" className="p-4" style={{ width: '100%' }}>
+            <Card.Body className="d-flex flex-column align-items-center">
+              <Card.Title>Playlists</Card.Title>
+              <ul className="list-unstyled">
+                {playlists.map((playlist) => (
+                  <li key={playlist.id} className="mb-2">
+                    <Button variant="outline-light" onClick={() => console.log(`Clicked playlist: ${playlist.name}`)}>
+                      {playlist.name}
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+    // <div>
+    //   <h2>Playlists:</h2>
+    //   <ul>
+    //     {playlists.map((playlist) => (
+    //       <li key={playlist.id}>
+    //         <button onClick={() => console.log(`Clicked playlist: ${playlist.name}`)}>
+    //           {playlist.name}
+    //         </button>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 };
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,41 @@
 // SearchBar.js - This component will contain the search functionality
 import React, { useState } from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 
 const SearchBar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
 
+
   const handleSearch = () => {
+
     onSearch(searchQuery); // Passes the search query to the parent component
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter music genres (e.g., country, metal, hip hop)"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
-    </div>
+    <Container className="d-flex justify-content-center align-items-center">
+      <Row>
+        <Col>
+          <Card bg="dark" text="white" className="p-4" style={{ width: '100vh'}}>
+            <Card.Body>
+              <Card.Title>Search</Card.Title>
+              <Form>
+                <Form.Group controlId="formSearch">
+                  <Form.Control 
+                    type="text" 
+                    placeholder="Enter music genres (e.g., country, metal, hip hop)" 
+                    className="border" 
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  />
+                </Form.Group>
+                <Button variant="success" onClick={handleSearch}>
+                  Search
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
