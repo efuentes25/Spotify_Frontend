3ce55d61da29107c6c8ee06f77764b24ecab9d22
Robin Fishman <rfishman@csumb.edu>
2023-11-20 12:51:27 -0800

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import PlaylistDisplay from './components/PlaylistDisplay';
 import SearchBar from './components/SearchBar';
 import MusicApp from './MusicApp';
@@ -13,46 +13,46 @@ const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
 
 const userTokens = {
-  get accessToken() { fetch(`${base_uri}/somethingsomething`).then(data => {return data.json()})},
-  get refreshToken() { fetch(`${base_uri}/somethingsomethingelse`).then(data => {return data.json()})}
-  // todo expires_in field
-  // todo expires field
-  // todo setters for above
+	get accessToken() { fetch(`${base_uri}/somethingsomething`).then(data => { return data.json() }) },
+	get refreshToken() { fetch(`${base_uri}/somethingsomethingelse`).then(data => { return data.json() }) }
+	// todo expires_in field
+	// todo expires field
+	// todo setters for above
 };
 
 async function redirectSpotifyOAuth() {
-  const string_pattern = process.env.REACT_APP_CODE_VERIFIER;
-  const hashed_values = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(string_pattern));
+	const string_pattern = process.env.REACT_APP_CODE_VERIFIER;
+	const hashed_values = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(string_pattern));
 
-  const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed_values)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
+	const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed_values)))
+		.replace(/=/g, '')
+		.replace(/\+/g, '-')
+		.replace(/\//g, '_');
 
-  window.localStorage.setItem('react_code_challenge', code_challenge_base64);
+	window.localStorage.setItem('react_code_challenge', code_challenge_base64);
 
-  const auth_uri = new URL(authorizationEndpoint);
-  const param = {
-    response_type: 'code',
-    client_id: process.env.REACT_APP_SPOTIFY_API_CLIENT_ID,
-    scope: scope,
-    code_challenge_method: 'S256',
-    code_challenge: code_challenge_base64,
-    redirect_uri: redirectUrl,
-  }
+	const auth_uri = new URL(authorizationEndpoint);
+	const param = {
+		response_type: 'code',
+		client_id: process.env.REACT_APP_SPOTIFY_API_CLIENT_ID,
+		scope: scope,
+		code_challenge_method: 'S256',
+		code_challenge: code_challenge_base64,
+		redirect_uri: redirectUrl,
+	}
 
-  auth_uri.search = new URLSearchParams(param).toString();
-  window.location.href = auth_uri.toString(); // redirects to spotify auth server for login
+	auth_uri.search = new URLSearchParams(param).toString();
+	window.location.href = auth_uri.toString(); // redirects to spotify auth server for login
 }
 
 function App() {
-  redirectSpotifyOAuth();
+	redirectSpotifyOAuth();
 
-  return (
-    <div>
-      <MusicApp />
-    </div>
-  );
+	return (
+		<div>
+			<MusicApp />
+		</div>
+	);
 }
 
 
